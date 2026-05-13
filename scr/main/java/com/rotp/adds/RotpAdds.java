926a3182cce35c871e7752024ef0b893eb9f3879
Solkree <densmola303@gmail.com>
2026-05-13 23:05:54 +0300

    Create RotpAdds.java

---
+++ scr/main/java/com/rotp/adds/RotpAdds.java
@@ -0,0 +1,10 @@
+package com.rotp.adds;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("rotp_adds")
+public class RotpAdds {
+    public RotpAdds() {
+        // Логика работает через встроенный датапак в папке resources/data.
+    }
+}
